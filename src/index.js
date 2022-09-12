e134d083d4bdb6ae674c3a82dbe7930b2cbb4967
fogarea <shiitakedd@gmail.com>
2022-09-12 23:55:28 +0300

    feat: add adaptive navigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles/style.scss";
 
-console.log('hello')
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelector('.header__nav');
+const navItem = document.querySelectorAll('.nav__item');
+
+// show hide navigation
+const handleBtnClick = () => {
+	nav.classList.remove('nav--show')
+	document.body.classList.remove('body--scroll__disable');
+};
+
+openBtn.addEventListener('click', () => {
+	nav.classList.add('nav--show')
+	document.body.classList.add('body--scroll__disable');
+	for (let item of navItem) {
+		item.addEventListener('click', handleBtnClick)
+	}
+});
+
+closeBtn.addEventListener('click', handleBtnClick);
